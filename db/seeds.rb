0864891289ffb2c6def98ff710a34d268ca1484b
Paul M2B <polo2bodi@gmail.com>
2018-10-15 11:17:34 +0200

    Create models Channel and Message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,33 @@ User.destroy_all
 
 puts "create 1 user: polo@polo.fr, 00000000"
 
-User.create!(email: "polo@polo.fr", password: "00000000")
+polo = User.create!(email: "polo@polo.fr", password: "00000000")
 
 puts "#{User.count} user created"
+
+Channel.destroy_all
+
+puts "create 2 channels: general, btach-109"
+
+general = Channel.create!(name: "general")
+batch_109 = Channel.create!(name: "batch-109")
+
+puts "#{Channel.count} channels created"
+
+Message.destroy_all
+
+puts "create 2 messages, one by channel"
+
+Message.create!(
+  channel: general,
+  user: polo,
+  content: "Bonjour général",
+)
+
+Message.create!(
+  channel: batch_109,
+  user: polo,
+  content: "Salut les 109!",
+)
+
+puts "#{Message.count} messages created"
